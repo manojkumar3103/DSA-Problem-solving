d383602913527b40b24af368ac3c33579101b3d1
manojkumar3103 <manojkumarrp217@gmail.com>
2025-02-13 14:48:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Pair Sum in BST/pair-sum-in-bst.cpp
@@ -0,0 +1,142 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+#define MAX_SIZE 100000
+
+// Tree Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/*
+Node is as follows
+class Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int pairSum(Node*root, int target,unordered_set<int>& hset)
+    {
+        if(!root) return -1;
+        int left = pairSum(root->left,target,hset);
+        if(left==1) return left;
+        if(hset.find(target-root->data)!=hset.end())
+            return 1;
+        hset.insert(root->data);
+        return pairSum(root->right,target,hset);
+    }
+    bool findTarget(Node *root, int target) {
+        unordered_set<int> hset;
+        return (pairSum(root,target,hset)==1);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+
+        getline(cin, s);
+        int k = stoi(s);
+        // getline(cin, s);
+        Solution obj;
+        cout << obj.findTarget(root, k) << endl;
+        // cout<<"~"<<endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends

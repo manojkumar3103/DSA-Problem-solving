a21a3d157b93f6d9d3962462b488db1176ac945a
Manojkumar Radhakrishnan <manojkumarrp217@gmail.com>
2025-06-11 12:55:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove the balls/remove-the-balls.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int findLength(vector<int> &color, vector<int> &radius) {
+        // code here
+        stack<pair<int, int>> st;
+        int n = color.size();
+        
+        for (int i = 0; i < n; ++i) {
+            if (!st.empty() && st.top().first == color[i] && st.top().second == radius[i]) {
+                st.pop();
+            } else {
+                st.push({color[i], radius[i]});
+            }
+        }
+        
+        return st.size();
+    }
+};
